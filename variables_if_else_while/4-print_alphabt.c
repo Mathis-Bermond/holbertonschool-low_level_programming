f946c3c3ce0b4e24f79c2f3a96114afab32c22eb
MBRorry <9985@holbertonstudents.com>
2024-10-22 17:16:47 +0200

    Test

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+  *main - Print alphabet without q or e
+  *
+  *Return: 0
+ */
+
+int main (void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+			putchar(c);
+		c++;
+	}
+
+	return (0);
+}
