7a77cd732bc59987c4ebb0e63e34a8f880f66519
MBRorry <9985@holbertonstudents.com>
2024-10-22 19:53:29 +0200

    Betty the good girl

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 /**
-  *main - Okay
-  *
-  *Return: 0
+ *main - Okay
+ *
+ *Return: 0
  **/
 
 int main(void)
 {
-        char i = '0';
+	char i = '0';
 
-                while (i <= '9')
-                {
-                        putchar(i);
-                        i++;
-                }
-                putchar('\n');
-                return (0);
+		while (i <= '9')
+		{
+			putchar(i);
+			i++;
+		}
+		putchar('\n');
+		return (0);
 }
 
 
