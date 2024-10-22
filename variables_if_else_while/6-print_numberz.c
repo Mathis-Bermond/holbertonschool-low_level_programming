458b46e2dec10b6df1f504d4d92e6305c8addae1
MBRorry <9985@holbertonstudents.com>
2024-10-22 19:49:39 +0200

    Test CTRL C

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+  *main - Okay
+  *
+  *Return: 0
+ **/
+
+int main(void)
+{
+        char i = '0';
+
+                while (i <= '9')
+                {
+                        putchar(i);
+                        i++;
+                }
+                putchar('\n');
+                return (0);
+}
+
+
+
+
+
+
