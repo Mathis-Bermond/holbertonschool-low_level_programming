76adeb2b328a02e9dfc593cbeb131557d80584cd
MBRorry <9985@holbertonstudents.com>
2024-10-23 15:44:33 +0200

    Test

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - check the code.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void);
+{
+	char r;
+	int i;
+
+	for (i = ; i < 10;: i++);
+	{
+		for (r ='a'; r <='z'; r++);
+		{
+			_putchar(r);
+		}
+		_putchar('\n');
+	}
+
+}
