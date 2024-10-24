f9acdfece240425d9a8a40fde65cf969682f3973
MBRorry <9985@holbertonstudents.com>
2024-10-24 09:26:29 +0200

    OK

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,14 @@
  * Return: Always 0.
  */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	char r;
 	int i;
 
-	for (i = ; i < 10;: i++);
+	for (i = '0'; i < 10; i++)
 	{
-		for (r ='a'; r <='z'; r++);
+		for (r ='a'; r <='z'; r++)
 		{
 			_putchar(r);
 		}
---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_islower - Teck is cool
+ *
+ *Return:1 if C is lower 
+ */
+
+int _islower(int c)
+{
+	if
+		{
+			return (1);
+		}
+	else
+		{
+			return (0);
+		}
+}
