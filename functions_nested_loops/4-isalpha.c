88c85bdd6f1e5c3debd0c8510b1b6f0b7ad6b533
MBRorry <9985@holbertonstudents.com>
2024-10-25 10:34:23 +0200

    Test

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/*
+*_isalpha - Test _isalpha
+*
+*@c: int
+*
+*Return: 1 or 0
+*/
+
+int _isalpha(int c)
+
+{
+	if (c >= 97 && c <= 122)
+
+	{
+		return (1);
+	}
+	else if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
