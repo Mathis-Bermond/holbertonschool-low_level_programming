8f5e0125b80a52d8962cf915bc585be57dc8d2df
MBRorry <9985@holbertonstudents>
2024-10-25 14:27:40 +0200

    ok

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - trying
+ * 
+ * @r: int
+ * 
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+	return (0);
+}
