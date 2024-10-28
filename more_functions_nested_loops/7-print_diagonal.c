0a4334c2dc3cf0a02d3518b7a70a9bd31d5c234f
MBRorry <9985@holbertonstudents.com>
2024-10-28 14:33:54 +0100

    Try

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_diagonal - Test
+* @n: OK
+* Return: 0
+*/
+
+void print_diagonal(int n)
+{
+	while (n > 0)
+	{
+		_putchar(' ');
+		n--;
+	}
+	_putchar('\\');
+	_putchar('\n');
+}
