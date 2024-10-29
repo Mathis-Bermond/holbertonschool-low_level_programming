94aa83054b5eca62e9d6cb7df312c63d7108686d
MBRorry <9985@holbertonstudents.com>
2024-10-29 10:33:53 +0100

    Test

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -2,25 +2,32 @@
 
 /**
 * print_diagonal - Test
-* @n: OK
+*@n: Test
+*@c
+*@i
 * Return: 0
 */
 
 void print_diagonal(int n)
-{
-	int i, n;
 
-	for (i = 0; i < n; i++)
-	{
-		for (n = 0; n < i; n++)
-		{
-			_putchar(' ');
-		}
-		_putchar('\\');
-		_putchar('\n');
-	}
+{
+	int i;
+	int c;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (c = 0; c < i; c++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
 	}
 }
