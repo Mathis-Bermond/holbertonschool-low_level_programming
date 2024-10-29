8c06574b4ab81a0580991f68a7a6be5f72f8a15d
MBRorry <9985@holbertonstudents.com>
2024-10-29 14:12:46 +0100

    Test

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+*FizzBuzz - Test
+*@i
+*
+*Return: Test
+*/
+
+void fizz_buzz(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i == 100)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(" ");
+		}
+	}
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	fizz_buzz();
+	return (0);
+}
