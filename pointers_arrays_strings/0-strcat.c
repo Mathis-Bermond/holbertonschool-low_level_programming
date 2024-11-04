327369c6c0b75d07a7f218616c7dccfa0cdf23a4
MBRorry <9985@holbertonstudents>
2024-11-04 14:08:41 +0100

    test

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+
+/**
+*_strcat - strcat
+*
+*@j
+*@i
+*@src: Test
+*@dest: TEst
+*Return: dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
