baf6e139eb78f4f517113067a0a63533960b0f74
MBRorry <9985@holbertonstudents>
2024-11-04 09:49:11 +0100

    TESt

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include "string.h"
+
+/**
+ * print_array - Test
+ *
+ * @n: test
+ * @a: Test
+ */
+
+void print_array(int *a, int n)
+
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
+	}
+	printf("\n");
+}
