ffb5018c2414e5b2addea284916ce52df9882d86
MBRorry <9985@holbertonstudents.com>
2024-11-05 13:50:38 +0100

    test

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,14 +1,23 @@
 #include "main.h"
-#include <string>
+#include <string.h>
 
 /**
- * _strcmp - test
- * 
- * @s1
- * 
- * @s2
- * 
- * 
- */
+* _strcmp - test
+*
+* @s1: test
+*
+* @s2: test
+*
+*Return: s1 / 2
+*/
 
 int _strcmp(char *s1, char *s2)
+{
+	for (; (*s1 != '\0' && *s2 != '\0') && (*s1 == *s2); s1++, s2++)
+		;
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	return (*s1 - *s2);
+}
