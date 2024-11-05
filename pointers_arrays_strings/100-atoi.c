1671d06a925ebfb0363fc104e37d32f996072848
MBRorry <9985@holbertonstudents.com>
2024-11-05 13:43:03 +0100

    test

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,18 +1,51 @@
 #include "main.h"
-#include <string.h>
 
 /**
-*
-*
-*
-*
-*/
+ * _atoi - convert a string to an integer
+ * @s: string to convert
+ * Return: integer value of string
+ */
 
-int main ()
-
+int _atoi(char *s)
 {
-	char *s1 = "";
+	int sign = 1;
+	int result = 0;
+	int started = 0;
 
-	_putchar(s1)
+	while (*s)
+	{
+		int MAX = 2147483647;
+		int MIN = -2147483648;
+
+		if (result > MAX / 10 || (result == MAX / 10 && (*s - '0') > MAX % 10))
+		{
+			if (sign == 1)
+				return (MAX);
+			else
+				return (MIN);
+		}
+		if (*s == '-')
+		{
+			if (started)
+				break;
+			sign = -sign;
+		}
+		else if (*s == '+')
+		{
+			if (started)
+				break;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			started = 1;
+			result = result * 10 + (*s - '0');
+		}
+		else if (started)
+		{
+			break;
+		}
+		s++;
+	}
+
+	return (result * sign);
 }
-
---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string>
+
+/**
+ * _strcmp - test
+ * 
+ * @s1
+ * 
+ * @s2
+ * 
+ * 
+ */
+
+int _strcmp(char *s1, char *s2)
