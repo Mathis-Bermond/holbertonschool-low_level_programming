6838c8d625fbf81afbafec84b74e9d23180d9c5f
MBRorry <9985@holbertonstudents.com>
2024-11-07 14:07:49 +0100

    test

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+*_puts_recursion - print string
+*
+*@s: string
+*@i: compteur
+*
+*return: 0
+*/
+
+void _puts_recursion(char *s)
+
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		return (0);
+	}
+	i++;
+	_putchar(s[i]);
+}
+
+/**
+ * main - return 0 or puts
+ * 
+ * return: 0
+ */
+
+int main ()
+
+{
+	if (s[i] == '\0')
+	{
+		return (0);
+	}
+	else
+	return (_puts_recursion);
+}
