9392d56ff3319feb803dad70d1d8684dc64ae2ad
MBRorry <9985@holbertonstudents.com>
2024-11-12 09:56:15 +0100

    test

---
+++ argc_argv/0-whatismyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - argc argv
+*
+* @argc:blop
+* @argv:
+*
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}
