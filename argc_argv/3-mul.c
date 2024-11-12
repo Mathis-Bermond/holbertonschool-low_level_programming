acc306c250689a51e81088128d5ef78e7803de94
MBRorry <9985@holbertonstudents.com>
2024-11-12 15:06:04 +0100

    Test

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Test
+*
+* @argc: Test
+* @argv: Test
+*
+* Return: 1 ou 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, mul;
+
+	if (argc != 3)
+	{
+		printf("Error");
+		return (1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	mul = (num1 * num2);
+
+	printf("%d\n", mul);
+	return (0);
+}
