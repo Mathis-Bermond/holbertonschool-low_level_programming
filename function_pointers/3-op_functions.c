487ed8955214ac3ee055e4c760373e514f41b454
MBRorry <9985@holbertonstudents>
2024-11-22 16:29:35 +0100

    test

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,20 +1,63 @@
-int op_add(int a, int b);
+/**
+ * op_add - adition
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: sum of two integers
+ */
+
+int op_add(int a, int b)
 {
 	return (a + b);
 }
-int op_sub(int a, int b);
+
+/**
+ * op_sub - substraction
+ * @a: int 1
+ * @b: int2
+ *
+ * Return: result
+ */
+int op_sub(int a, int b)
 {
 	return (a - b);
 }
-int op_mul(int a, int b);
+
+/**
+ * op_mul - multiplication
+ * @a: int 1
+ * @b: int2
+ *
+ * Return: result
+ */
+
+int op_mul(int a, int b)
 {
 	return (a * b);
 }
-int op_div(int a, int b);
+
+/**
+ * op_div - division
+ * @a: int 1
+ * @b: int2
+ *
+ * Return: result
+ */
+
+int op_div(int a, int b)
 {
 	return (a / b);
 }
-int op_mod(int a, int b);
+
+/**
+ * op_mod - modulate
+ * @a: int 1
+ * @b: int2
+ *
+ * Return: result
+ */
+
+int op_mod(int a, int b)
 {
 	return (a % b);
 }
