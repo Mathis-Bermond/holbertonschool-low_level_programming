441fe8b981f21e5a20fd3bd4443b53b1c4c747d3
MBRorry <9985@holbertonstudents>
2024-11-22 15:35:58 +0100

    test

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -10,7 +10,7 @@
 
 int (*get_op_func(char *s))(int, int)
 {
-op_t ops[] = {
+	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
@@ -18,17 +18,15 @@ op_t ops[] = {
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-{
-	int i;
+	int i = 0;
 
-	while (ops[i].op != NULL; i++)
+	while (ops[i].op != NULL)
 	{
-		if (*s == ops[i].op && (*s + 1) != '\0')
+		if (*(ops[i].op) == *s)
 		{
 			return (ops[i].f);
 		}
-		exit(98);
+		i++;
 	}
 	return (NULL);
-}
 }
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -3,25 +3,40 @@
 #include <stdlib.h>
 
 /**
-* main - Calculatrice
-*
-*@argc: argument
-*@argv: array
-*
-*Return: résulta
-*/
+ * main - entry point for function
+ * @argc: arg counter, number of args
+ * @argv: array of pointers to args
+ *
+ * Description: calculates basic arithmetic
+ *
+ * Return: 0 on success, 98 on failure
+ */
 
 int main(int argc, char *argv[])
 {
-	int result = 0;
+	char op;
+	int (*op_func)(int, int);
+	int result;
 
-	int (*tmp)(int, int);
-	int num1 = atoi(argv[1]);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	op = *(argv[2]);
+	if ((op == '/' || op == '%') && !atoi(argv[3]))
+	{
+		printf("Error\n");
+		return (100);
+	}
 
-	int num2 = atoi(argv[3]);
-
-	tmp = &get_op_func;
-	result = (*tmp)(num1, num2);
-
-	return (result);
+	op_func = get_op_func(&op);
+	if (op_func == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		return (99);
+	}
+	result = op_func(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", result);
+	return (0);
 }
